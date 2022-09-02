85faf5f030c1e38e3fc0abbce4f259af0ae7afb6
kany <kany102030@gmail.com>
2022-09-02 18:16:20 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const routes = require('./routes')
 const app = express()
 const exphbs = require('express-handlebars')
-const routes = require('./routes')
+
+
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.use(routes)
-
 const port = process.env.PORT
 
 app.listen(port, () => {
