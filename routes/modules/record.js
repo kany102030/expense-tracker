86436553592650a3bc45a449eec51ec0cf6fea16
kany <kany102030@gmail.com>
2022-09-03 23:29:15 +0800

    feat: add edit record function

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -2,6 +2,23 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+router.get('/:id/edit', (req, res) => {
+  Category.find()
+    .lean()
+    .then(categories => {
+      Record.findById(req.params.id)
+        .lean()
+        .then(record => {
+          const categorySelected = categories.find(category => String(category._id) === String(record.categoryId))
+          categoriesOther = categories.filter(category => category.name !== categorySelected.name)
+          record.date = record.date.toJSON().slice(0, 10)
+          res.render('edit', { record, categoriesOther, categorySelected })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+
+})
 router.get('/new', (req, res) => {
 
   Category.find()
@@ -11,6 +28,19 @@ router.get('/new', (req, res) => {
     })
     .catch(error => console.log(error))
 
+})
+router.put('/:id', (req, res) => {
+  const { record_name, record_date, record_category, record_price } = req.body
+  Record.findById(req.params.id)
+    .then(record => {
+      record.name = record_name
+      record.date = record_date
+      record.categoryId = record_category
+      record.amount = record_price
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 router.delete('/:id', (req, res) => {
   Record.findById(req.params.id)
